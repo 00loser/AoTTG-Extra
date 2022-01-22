6ad5b1e6825cac60eaf0b60a79dd0cdf170452c0
Sopas-ugu <91762103+Sopas-ugu@users.noreply.github.com>
2022-01-22 17:39:43 -0300

    3

---
+++ Commands/Clear.cs
@@ -0,0 +1,15 @@
+if (inputLine == "/clear")
+{
+    if (PhotonNetwork.isMasterClient)
+    {
+        for (int i = 0; i <= 14; i++)
+        {
+            string text = (i == 14) ? "Chat cleaned":string.Empty;
+        }
+    }
+    else
+    {
+        //FengGameManagerMKII.instance.chatRoom.addLINE("Error: You aren't masterclient");
+        addLINE("Error: You aren't masterclient");
+    }
+}
